69c01baa431fc5170b822b46cbfd00a3fb17a5c5
Vince Ng <android@ngvincent.com>
2014-05-11 15:41:41 -0400

    Updated vk810.mk

--- vk810.mk
+++ vk810.mk
@@ -1,24 +1,13 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
-$(call inherit-product-if-exists, vendor/lge/vk810/vk810-vendor.mk)
-
-DEVICE_PACKAGE_OVERLAYS += device/lge/vk810/overlay
-
-LOCAL_PATH := device/lge/vk810
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
-$(call inherit-product, build/target/product/full.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_vk810
 PRODUCT_DEVICE := vk810
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LGE_VK810
+PRODUCT_MANUFACTURER := LGE
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/lge/v500/device.mk)
+$(call inherit-product-if-exists, vendor/lge/vk810/vk810-vendor.mk)
+
