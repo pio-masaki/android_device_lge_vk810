995614d2decdf5060f5916dd0a3bb8684574ac88
Vince Ng <android@ngvincent.com>
2014-05-11 15:24:11 -0400

    Modified BoardConfig.mk to use the BoardConfigCommon.mk found in device/lge/v500/

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,30 +1,36 @@
-USE_CAMERA_STUB := true
+#
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# inherit from the proprietary version
--include vendor/lge/vk810/BoardConfigVendor.mk
-
-TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
-ARCH_ARM_HAVE_TLS_REGISTER := true
-
-TARGET_BOOTLOADER_BOARD_NAME := vk810
-
+# Try to build the kernel
+TARGET_KERNEL_SOURCE := kernel/lge/vk810
+#TARGET_PREBUILT_KERNEL := device/lge/vk810/kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 lpj=67677 androidboot.hardware=altev vmalloc=600M
-BOARD_KERNEL_BASE := 0x80200000
-BOARD_KERNEL_PAGESIZE := 2048
+TARGET_KERNEL_CONFIG := cyanogenmod_vk810_defconfig
 
-# fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_BOOTLOADER_BOARD_NAME := altev
+TARGET_BOOTLOADER_NAME=vk810
 
-TARGET_PREBUILT_KERNEL := device/lge/vk810/kernel
+BOARD_BOOTIMAGE_PARTITION_SIZE := 25165824 # 24M
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 25165824 # 24M
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2248146944 # 2114M
 
-BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 11517558784 # 10984M
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+
+TARGET_RECOVERY_FSTAB = device/lge/vk810/fstab.gvar
+
+# Include the config file common to all variants of the LG G Pad
+include device/lge/v500/BoardConfigCommon.mk
